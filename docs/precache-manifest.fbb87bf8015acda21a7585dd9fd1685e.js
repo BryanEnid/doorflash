43b880a00c5cc1752c9f1f5e1ca88a521fe1c736
Bryan Tejada <bryanenid97@gmail.com>
2019-08-18 22:27:44 -0400

    first commit

---
+++ docs/precache-manifest.fbb87bf8015acda21a7585dd9fd1685e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e872fa434822b3cfc06e068013e051d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a4b1e35d94dd82581e",
+    "url": "/static/css/main.41150668.chunk.css"
+  },
+  {
+    "revision": "e5d93d925506aa9a222e",
+    "url": "/static/js/2.3b1b0df1.chunk.js"
+  },
+  {
+    "revision": "83a4b1e35d94dd82581e",
+    "url": "/static/js/main.ee9a7bde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
